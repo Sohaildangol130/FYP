a3070feacf4fd0cff9afe66769733d14b3ede00e
Sohaildangol130 <sohaildangol130@gmail.com>
2021-03-11 22:26:46 +0545

    refined authentication design and validation process

--- Chitra/authentication/views.py
+++ Chitra/authentication/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 import random
 # Create your views here.
@@ -7,11 +8,12 @@ def login(request):
         username = request.POST['username']
         password = request.POST['password']
         user = auth.authenticate(username=username, password=password)
-        print (username, password)
         if user is not None:
             auth.login(request, user)
             return redirect('/')
-        return render(request, 'login.html')
+        else:
+            messages.info(request, "Username or password is incorrect.")
+            return render(request, 'login.html')
     else:
         return render(request, 'login.html')
 
@@ -21,11 +23,14 @@ def signup(request):
         last_name = request.POST['last_name']
         email = request.POST['email']
         password = request.POST['password']
-
-        if User.objects.filter(username=first_name).exists():
-            print ("username taken")
-        elif User.objects.filter(email=email).exists():
-            print ("email taken")
+        repassword = request.POST['re_password']
+        
+        if (User.objects.filter(email=email).exists()):
+            messages.info(request, "The email is already taken. Please try logging in with it.")
+            return render(request, 'register.html')
+        elif (password != repassword):
+            messages.info(request, "The passwords do not match. Please try it again.")
+            return render(request, 'register.html')
         else:
             user = User.objects.create_user(username=first_name + str(random.randint(0,99999)), first_name=first_name, last_name=last_name, email=email, password=password)
             user.save()
