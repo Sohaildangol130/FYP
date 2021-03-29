9f1f5d2a41fb08314d70f36e47c7709c2bb72cf5
Sohaildangol130 <sohaildangol130@gmail.com>
2021-03-29 09:11:43 +0545

    created search feature

--- Chitra/home/urls.py
+++ Chitra/home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import (index)
+from .views import index, search
 
 urlpatterns = [
-    path('', index.as_view())
+    path('', index.as_view()),
+    path('search', search)
 ]
--- Chitra/home/views.py
+++ Chitra/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from posts.models import all_posts
+from django.contrib import messages
 
 from django.views.generic import (
     CreateView,
@@ -12,3 +13,11 @@ from django.views.generic import (
 class index(ListView):
     queryset = all_posts.objects.all()
     template_name = 'index.html'
+
+def search(request):
+    search_text = request.GET['search']
+    # print(search_text)
+    search_filter = all_posts.objects.filter(post_name__icontains=search_text)
+    
+    messages.info(request, "Related posts for " + search_text)
+    return render(request, 'index.html', {'object_list': search_filter})
