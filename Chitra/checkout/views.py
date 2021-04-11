acbf445ca171a5ef5b18c33c3b73dd420c00da83
Sohaildangol130 <sohaildangol130@gmail.com>
2021-04-11 21:34:12 +0545

    created email feature on checkout

--- Chitra/checkout/views.py
+++ Chitra/checkout/views.py
@@ -6,11 +6,14 @@ from .models import checkout
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from django.core.mail import send_mail, EmailMultiAlternatives
+from django.template.loader import render_to_string
 
 def order(request):
     if (request.user.is_authenticated):
         if request.method == 'POST':
             chars = "[]"
+            all_products = []
             posts = request.POST['posts']
             if (posts):
                 phone_number = request.POST['phone_number']
@@ -25,8 +28,18 @@ def order(request):
                     user = request.user
                     for i in products:
                         product_details = all_posts.objects.get(id=int(i))
+                        all_products.append(product_details)
                         order = checkout(product=product_details, user=user, phone_number=phone_number, zone=zone, state=state, city=city, zip_code=zip_code )
                         order.save()
+                msg_html = render_to_string('email.html', {'first_name': request.user.first_name, 'all_products': all_products})
+                send_mail(
+                'Order from Chitra',
+                'You have ordered some items from Chitra. They will be delivered to you with this week.',
+                'chitra.fyp@gmail.com',
+                [request.user.email],
+                html_message=msg_html,
+                fail_silently=False
+                )
                 response = HttpResponseRedirect('/')
                 response.delete_cookie('items')
                 messages.success(request, "Your order has been placed!!")
